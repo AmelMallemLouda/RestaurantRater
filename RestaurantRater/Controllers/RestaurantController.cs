453d94b2aae781e442c1eeaef275bcd9368bdd74
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-01-17 21:31:21 -0500

    first commit

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,41 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
+
+        //Post(create)
+        //api/Restaurant
+        [HttpPost]
+        public async Task<IHttpActionResult> PostRestaurant([FromBody]Restaurant model )
+        {
+
+            if(model is null)
+            {
+                return BadRequest("Your request body cannot be empty");
+
+            }
+
+            //If thr model is valid
+            if (ModelState.IsValid)
+            {
+                //Store the model in the database
+                _context.Restaurants.Add(model);
+
+                int changeCoun=await _context.SaveChangesAsync();
+                return Ok("Your restaurant was created");
+            }
+            //The model is not valid,go ahead and reject it
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ RestaurantRater/Models/RestaurantDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class RestaurantDbContext:DbContext  // will be creating DataBase through the DbContext class and make changes
+    {
+        //The Class base is DbContext
+        public RestaurantDbContext():base("DefaultConnection")
+        {
+
+        }
+
+
+        // The DbSet represent a collection of entities in the context
+        public DbSet<Restaurant> Restaurants { get; set; } // The restaurant will have a table inside the database
+    }
+}
